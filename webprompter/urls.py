818c62330ad09a2659b7c0957f0c317f91058af2
Aaron <aaron@olkin.net>
2013-09-08 18:27:28 -0400

    Added google site verification support

--- webprompter/urls.py
+++ webprompter/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,9 +7,13 @@ admin.autodiscover()
 
 from . import views
 
+def googleverify(request,extra):
+    return HttpResponse("google-site-verification: google{}.html".format(extra))
+
 urlpatterns = patterns(
     '',
     url(r'^$', views.root),
+    url(r'^google([a-z0-9]{16}).html$', googleverify),
 
     url(r'^authorized/', views.authorized),
     url(r'^auth-error/', views.auth_error),
